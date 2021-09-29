48d284606656c7798d0b1de6368d19ce26741032
jurasach <jurasach@mail.ru>
2021-09-29 18:30:16 +0300

    Initial commit

---
+++ shop/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Category, Product
+from cart.forms import CartAddProductForm
+
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+
+    paginator = Paginator(products, 3)
+    page = request.GET.get('page')
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
+    return render(request, 'shop/product/list.html',
+                  {'page': page,
+                   'category': category,
+                   'categories': categories,
+                   'products': products})
+
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    cart_product_form = CartAddProductForm()
+    return render(request, 'shop/product/detail.html', {'product': product,
+                                                        'cart_product_form':cart_product_form})
